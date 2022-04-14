5289704766250147eb5bff9085d8e18d831a5ae5
email <pavloborisov44@gmail.com>
2022-04-15 02:48:47 +0300

    class function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <Car />
+        <Car />
+        <Car />
+      </>
   );
 }
 
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: 'blue',
+      name: "Pasha",
+      model : 'first'
+    }
+  }
+  onClickChange(){
+    const color = ['blue', 'yellow', 'red', 'green','grey']
+    const name = ['Arthur', 'Pasha', 'Dasha', 'Masha','Andriy']
+    const model = ['Hyper', 'Razer', 'Razen', 'Plya','Lexus']
+    let randomColor = Math.floor(Math.random() * color.length)
+    let randomName = Math.floor(Math.random() * name.length)
+    let randomModel = Math.floor(Math.random() * model.length)
+    this.setState({
+      color:color[randomColor],
+      name:name[randomName],
+      model:model[randomModel]
+    })
+}
+  render(){
+    return(<h2 onClick = {this.onClickChange.bind(this)}>I love color - {this.state.color}, Name - {this.state.name}, Model - {this.state.model} </h2>)
+  }
+}
 export default App;
+
